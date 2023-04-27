5827f1fde0ecd0a0b0973561566ff44d1c00ae82
Qiming Yu <qiming.yu@agile-robots.com>
2023-04-27 12:00:06 +0200

    update setbuf function to automatically delete cretaed template file

--- 5-stdio/setbuf_customized.c
+++ 5-stdio/setbuf_customized.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define RWXRWXRWX (S_IRWXU|S_IRWXG|S_IRWXO)
 
 void setbuf_custmized(FILE *restrict fp, char *restrict buf ) {
     if(!buf) {
@@ -13,16 +17,25 @@ void setbuf_custmized(FILE *restrict fp, char *restrict buf ) {
 }
 
 int main(int argc, char* argv[]) {
-    if(argc < 2) {
-        fprintf(stderr, "Usage: ./a.out <filename>\n");
+    const char* tmpfile = "tempfile";
+    if(creat(tmpfile, RWXRWXRWX) < 0) {
+        fprintf(stderr, "Error creating temp file\n");
         return -1;
     }
-    FILE* fp = fopen(argv[1], "r+");
+    FILE* fp = fopen(tmpfile, "r+");
     if(!fp) {
-        fprintf(stderr, "Error Opening file %s\n", argv[1]);
+        fprintf(stderr, "Error Opening %s\n", tmpfile);
         return -1;
     }
+    if (unlink(tmpfile) < 0) {
+        fprintf(stderr, "Error unlinking temp file\n");
+        return -1;
+    }
+    
     setbuf_custmized(fp, NULL);
+    if(fp->_flags & _IO_UNBUFFERED) fprintf(stdout, "file unbuffered\n");
+    else if(fp->_flags & _IO_LINE_BUF) fprintf(stdout, "file linebuffered\n");
+    else fprintf(stdout, "file fullbuffered\n");
     fclose(fp);
     return 0;
 }
