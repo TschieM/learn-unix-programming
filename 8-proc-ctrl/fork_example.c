c06fa6de931f294d575241e8f7c39cf9681390cc
Qiming Yu <qiming.yu@agile-robots.com>
2023-04-28 10:21:34 +0200

    add comment

--- 8-proc-ctrl/fork_example.c
+++ 8-proc-ctrl/fork_example.c
@@ -8,10 +8,23 @@ static char* buf = "hello world!\n";
 int main(int argc, char* argv[]) {
     int var = 100;
 
+    // write function is not buffered, so this output should only happen once
     if(write(STDOUT_FILENO, buf, 13) != 13) {
         fprintf(stderr, "Error writing to terminal\n");
         exit(-1);
     }
+
+    // printf is a std io, when it is printed to output(terminal) then it is line buffered
+    // In this case, the buffer will be flushed when when it hits the end of the line, so 
+    // This print will be printed once
+    // run with: ./a.out
+
+    // However, if the output is redirected to a file(when the file pointer is not pointing to the terminal output)
+    // then it will be fully buffered, after the buffer will not be flushed until it is full or the process terminates
+    // when fork is called, the child process will copy the buffer(which contains the message). Thus the message will
+    // show in the file twice
+    // run with: ./a.out > out.file
+    printf("Hey there, this is a buffered output\n");
 
     pid_t pid = fork();
     if(pid < 0) {
@@ -21,7 +34,7 @@ int main(int argc, char* argv[]) {
         g_var *= 2;
         var++;
     } else {
-        sleep(5);
+        sleep(2);
     }
 
     fprintf(stdout, "pid-%d: g_var = %d, var = %d\n", getpid(), g_var, var);
